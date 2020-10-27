088e1ae5994bc3f4c453afcd8355a566d361c591
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-10-27 08:26:12 +0100

    adding number format exception handling

--- src/test/java/com/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -59,6 +59,14 @@ class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         mockMvc.perform(get("/recipe/new"))
                 .andExpect(status().isOk())
