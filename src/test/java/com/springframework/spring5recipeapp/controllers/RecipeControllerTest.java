d777a709fa353de1b16d51bd22a5aaeb34057a9e
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-10-27 21:56:20 +0100

    small test fix

--- src/test/java/com/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -79,15 +79,17 @@ class RecipeControllerTest {
     public void testPostNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
         command.setId(2L);
+
         when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
                 .param("directions", "some directions")
         )
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/recipe/2/show"));
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"));
     }
 
     @Test
